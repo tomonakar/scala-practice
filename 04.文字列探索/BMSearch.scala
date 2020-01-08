c496d25848306e2c4dc603f18775b5ee2ff96e2c
tomonakar <tomohisa.nak@gmail.com>
2020-01-08 12:53:22 +0900

    add partial sum depth first search

--- 04.文字列探索/BMSearch.scala
+++ 04.文字列探索/BMSearch.scala
@@ -2,8 +2,7 @@
 // https://ja.wikipedia.org/wiki/%E3%83%9C%E3%82%A4%E3%83%A4%E3%83%BC-%E3%83%A0%E3%83%BC%E3%82%A2%E6%96%87%E5%AD%97%E5%88%97%E6%A4%9C%E7%B4%A2%E3%82%A2%E3%83%AB%E3%82%B4%E3%83%AA%E3%82%BA%E3%83%A0
 
 object BMSearch extends App {
-  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq }
-  val pattern = "ドワンゴ".toSeq
+  val text = "カワカドカドカドドワンゴカドカドンゴドワドワンゴドワカワカドンゴドワ".toSeq val pattern = "ドワンゴ".toSeq
   val skipTable = pattern.map(s => (s -> (pattern.reverse.indexOf(s)))).toMap
 
   val matchIndexes = search(text, pattern)
