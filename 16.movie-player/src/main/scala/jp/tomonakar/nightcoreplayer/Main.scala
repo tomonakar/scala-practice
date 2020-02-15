df3fd0207a0cbe68add64629c2008b4b3ff169c9
tomonakar <tomohisa.nak@gmail.com>
2020-02-15 17:21:14 +0900

    feat: update media player

--- 16.movie-player/src/main/scala/jp/tomonakar/nightcoreplayer/Main.scala
+++ 16.movie-player/src/main/scala/jp/tomonakar/nightcoreplayer/Main.scala
@@ -1,8 +1,10 @@
 package jp.tomonakar.nightcoreplayer
 
+import java.io.File
 import javafx.application.Application
 import javafx.scene.Scene
 import javafx.scene.layout.BorderPane
+import javafx.scene.media.{Media, MediaPlayer, MediaView}
 import javafx.scene.paint.Color
 import javafx.stage.Stage
 
@@ -13,7 +15,15 @@ object Main extends App {
 class Main extends Application {
 
   override def start(primaryStage: Stage): Unit = {
+    val path = "/Users/staff018/Desktop/video.mp4"
+    val media = new Media(new File(path).toURI.toString)
+    val mediaPlayer = new MediaPlayer(media)
+    mediaPlayer.setRate(1.25)
+    mediaPlayer.play()
+    val mediaView = new MediaView(mediaPlayer)
     val baseBorderPane = new BorderPane()
+    baseBorderPane.setStyle("-fx-background-color: Black")
+    baseBorderPane.setCenter(mediaView)
     val scene = new Scene(baseBorderPane, 800, 500)
     scene.setFill(Color.BLACK)
     primaryStage.setScene(scene)
