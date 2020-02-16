9e586f754f014dd074b7c4275650cd7f625489c8
tomonakar <tomohisa.nak@gmail.com>
2020-02-16 18:01:14 +0900

    feat: add movie time view

--- 16.movie-player/src/main/scala/jp/tomonakar/nightcoreplayer/Main.scala
+++ 16.movie-player/src/main/scala/jp/tomonakar/nightcoreplayer/Main.scala
@@ -2,11 +2,15 @@ package jp.tomonakar.nightcoreplayer
 
 import java.io.File
 import javafx.application.Application
+import javafx.beans.value.{ChangeListener, ObservableValue}
+import javafx.geometry.Pos
 import javafx.scene.Scene
-import javafx.scene.layout.BorderPane
+import javafx.scene.control.Label
+import javafx.scene.layout.{BorderPane, HBox}
 import javafx.scene.media.{Media, MediaPlayer, MediaView}
 import javafx.scene.paint.Color
 import javafx.stage.Stage
+import javafx.util.Duration
 
 object Main extends App {
   Application.launch(classOf[Main], args: _*)
@@ -18,16 +22,58 @@ class Main extends Application {
     val path = "/Users/staff018/Desktop/video.mp4"
     val media = new Media(new File(path).toURI.toString)
     val mediaPlayer = new MediaPlayer(media)
+
     mediaPlayer.setRate(1.25)
     mediaPlayer.play()
+
     val mediaView = new MediaView(mediaPlayer)
+    mediaView.setFitWidth(800)
+    mediaView.setFitHeight(450)
+
+    val timeLabel = new Label()
+    mediaPlayer
+      .currentTimeProperty()
+      .addListener(new ChangeListener[Duration] {
+        override def changed(observable: ObservableValue[_ <: Duration],
+                             oldValue: Duration,
+                             newValue: Duration): Unit =
+          timeLabel.setText(
+            formatTime(mediaPlayer.getCurrentTime, mediaPlayer.getTotalDuration)
+          )
+      })
+    mediaPlayer.setOnReady(new Runnable {
+      override def run(): Unit =
+        timeLabel.setText(
+          formatTime(mediaPlayer.getCurrentTime, mediaPlayer.getTotalDuration)
+        )
+    })
+
+    timeLabel.setText("00:00:00/00:00:00")
+    timeLabel.setTextFill(Color.WHITE)
+    val toolBar = new HBox(timeLabel)
+    toolBar.setAlignment(Pos.CENTER)
+    toolBar.setStyle("-fx-background-color: Black")
+
     val baseBorderPane = new BorderPane()
     baseBorderPane.setStyle("-fx-background-color: Black")
     baseBorderPane.setCenter(mediaView)
+    baseBorderPane.setBottom(toolBar)
+
     val scene = new Scene(baseBorderPane, 800, 500)
     scene.setFill(Color.BLACK)
     primaryStage.setScene(scene)
     primaryStage.show()
+  }
+  private[this] def formatTime(elapsed: Duration,
+                               duration: Duration): String = {
+    "%02d:%02d:%02d/%02d:%02d:%02d".format(
+      elapsed.toHours.toInt,
+      elapsed.toMinutes.toInt % 60,
+      elapsed.toSeconds.toInt % 60,
+      duration.toHours.toInt,
+      duration.toMinutes.toInt % 60,
+      duration.toSeconds.toInt % 60
+    )
   }
 
 }
